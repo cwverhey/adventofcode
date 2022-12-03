eae6c4fd4705d819c7dd6d17d1df0a4879a67f08
Caspar Verhey <caspar@verhey.net>
2022-12-03 09:01:24 +0100

    day03.R

--- 2022/day03.R
+++ 2022/day03.R
@@ -5,14 +5,24 @@ source('functions.R')
 # part 1 ------------------------------------------------------------------
 
 year = 2022
-day = # ...
+day = 3
 part = 1
 
 task(year, day, part)
 
-src <- input(year, day)
+src <- input(year, day, splitlines = T)
 
-# ...
+letterValues <- 1:52
+names(letterValues) <- c(letters[1:26],LETTERS[1:26])
+
+overlapValues <- sapply(src, \(x) {
+  x <- strsplit(x, '', T)[[1]]
+  x <- split(x, rep(c(1, 2), each = length(x) / 2))
+  overlap <- intersect(x[[1]], x[[2]])
+  return(letterValues[overlap])
+})
+
+answer <- sum(overlapValues)
 
 submit(answer, year, day, part)
 
@@ -23,7 +33,15 @@ part = 2
 
 task(year, day, part)
 
-# ...
+elfGroups <- split(src, rep(seq(length(src)/3), each = 3))
+
+overlapValues <- sapply(elfGroups, \(x) {
+  x <- strsplit(x, '')
+  overlap <- intersect(intersect(x[[1]], x[[2]]), x[[3]])
+  return(letterValues[overlap])
+})
+
+answer <- sum(overlapValues)
 
 submit(answer, year, day, part)
 
