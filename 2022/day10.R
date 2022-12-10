f4cf159560be5478e16453be06c8ffe40bd96f99
Caspar Verhey <caspar@verhey.net>
2022-12-11 00:49:09 +0100

    day10.R

---
+++ 2022/day10.R
@@ -0,0 +1,82 @@
+setwd(dirname(rstudioapi::getSourceEditorContext()$path))
+source('functions.R')
+
+
+# part 1 ------------------------------------------------------------------
+
+year = 2022
+day = 10
+part = 1
+
+task(year, day, part)
+
+src <- input(year, day, splitlines = T)
+
+cycle <- 1
+X <- 1
+Xlog <- data.frame(cycle = cycle, X = X)
+
+for(command in src) {
+
+  if(command == "noop") {
+    cycle <- cycle + 1
+    next
+  }
+
+  if(startsWith(command, "addx ")) {
+    cycle <- cycle + 2
+    X <- X + strsplit(command, ' ', fixed=T)[[1]][2] |> as.numeric()
+    Xlog[nrow(Xlog) + 1,] = list(cycle, X)
+  }
+}
+
+signal_strengths <- sapply(seq(20, 220, 40),
+                           \(x) {
+                             x * Xlog$X[Xlog$cycle <= x] |> tail(n = 1)
+                           })
+
+answer <- sum(signal_strengths)
+
+submit(answer, year, day, part)
+
+
+# part 2 ------------------------------------------------------------------
+
+part = 2
+
+task(year, day, part)
+
+X <- 1
+Xlog <- c(X)
+
+for(cmd in src) {
+  Xlog <- c(Xlog, X)
+  if(cmd != "noop") {
+    X <- X + (strsplit(cmd, ' ', fixed=T)[[1]][2] |> as.numeric())
+    Xlog <- c(Xlog, X)
+  }
+}
+
+for(cycle in 1:240) {
+
+  crt_position <- (cycle -1) %% 40
+
+  cat(
+    ifelse( abs(crt_position - Xlog[cycle]) <= 1,
+          '⬛️',
+          '⬜️')
+  )
+
+  if(cycle%%40 == 0) cat('\n')
+}
+
+answer <- "ZUPRFECL"
+
+submit(answer, year, day, part)
+
+
+# git push ----------------------------------------------------------------
+
+system(sprintf('git add "%s"', basename(rstudioapi::getSourceEditorContext()$path)))
+system(sprintf('git commit -m "%s"', basename(rstudioapi::getSourceEditorContext()$path)))
+system('git push')
