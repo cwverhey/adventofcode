a56f09459d4c0313f7da2a1a0fec5f7f2a768780
Caspar Verhey <caspar@verhey.net>
2022-12-11 01:01:14 +0100

    day04.R

--- 2022/day04.R
+++ 2022/day04.R
@@ -10,7 +10,7 @@ part = 1
 
 task(year, day, part)
 
-src <- input(year, day, splitlines = T)
+src <- input(year, day)
 
 overlap <- sapply(src, \(x) {
   x <- strsplit(x, ',', fixed=T)[[1]]
@@ -48,6 +48,4 @@ submit(answer, year, day, part)
 
 # git push ----------------------------------------------------------------
 
-system(sprintf('git add "%s"', basename(rstudioapi::getSourceEditorContext()$path)))
-system(sprintf('git commit -m "%s"', basename(rstudioapi::getSourceEditorContext()$path)))
-system('git push')
+git_push()
