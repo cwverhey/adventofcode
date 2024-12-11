6f7010b1213c3795c4db66e20f9fe138feebfc1a
Caspar Verhey <caspar@verhey.net>
2024-12-11 17:45:49 +0100

    mega-optimized pt2

--- 2024/day11.py
+++ 2024/day11.py
@@ -38,13 +38,51 @@ aoc.submit(2024, 11, 1, stones)
 aoc.task(2024, 11, 2)
 
 from collections import defaultdict
+from math import log10
 
 #inputs = [125, 17]  # correct answer: 55312 @ 25 blinks
 inputs = aoc.get_input(2024, 11, numeric=True)
 
+def digs(num):
+  '''faster than int(log10(num))+1'''
+  if num < 10:
+    return 1
+  elif num < 100:
+    return 2
+  elif num < 1000:
+    return 3
+  elif num < 10_000:
+    return 4
+  elif num < 100_000:
+    return 5
+  elif num < 1_000_000:
+    return 6
+  elif num < 10_000_000:
+    return 7
+  elif num < 100_000_000:
+    return 8
+  elif num < 1_000_000_000:
+    return 9
+  elif num < 10_000_000_000:
+    return 10
+  elif num < 100_000_000_000:
+    return 11
+  elif num < 1_000_000_000_000:
+    return 12
+  else:
+    return -1
+
+def blink_num(num):
+    if num == 0:
+        return [1]
+    digits = digs(num)
+    if digits % 2 == 0:
+        divisor = 10**(digits//2)
+        return [ num // divisor, num % divisor ]
+    return [ num*2024 ]
+
 def blink(nums, times):
     for i in range(times):
-        print(f'blink {i+1}:')
         result = defaultdict(lambda: 0)
         for n_in, count in nums.items():
             for n_out in blink_num(n_in):
