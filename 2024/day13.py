2d25d70d31ae047d7ecefdb0f83585509314cab7
Caspar Verhey <caspar@verhey.net>
2024-12-13 20:58:52 +0100

    day 13

--- 2024/day13.py
+++ 2024/day13.py
@@ -46,13 +46,30 @@ aoc.submit(2024, 13, 1, total_price)
 
 aoc.task(2024, 13, 2)
 
-inputs = aoc.examples(2024, 13)[0]  # correct answer: ____
-#inputs = aoc.get_input(2024, 13)
+#inputs = aoc.examples(2024, 13, lines=True)[0]  # correct answer: only on 2nd and 4th
+inputs = aoc.get_input(2024, 13, lines=True)
 
-____
+machines = []
+for line in inputs:
+    if line.startswith('Button A'):
+        a = tuple(int(x) for x in re.findall(r'\d+', line))
+    elif line.startswith('Button B'):
+        b = tuple(int(x) for x in re.findall(r'\d+', line))
+    elif line.startswith('Prize'):
+        target = tuple(int(x) for x in re.findall(r'\d+', line))
+        machines.append(tuple([a, b, target]))
+
+total_price = 0
+for m in machines:
+    A, B = sympy.symbols('A B')
+    eqs = [ sympy.Eq( m[0][i]*A + m[1][i]*B, m[2][i] + 10_000_000_000_000 ) for i in range(2)]
+    solution = sympy.solve(eqs, (A, B))
+    print(eqs, solution)
+    if all(isinstance(x, sympy.core.numbers.Integer) for x in solution.values()):
+        total_price += solution[A]*3+solution[B]
 
 # submit answer
-aoc.submit(2024, 13, 2, ____)
+aoc.submit(2024, 13, 2, total_price)
 
 # push to git
 aoc.push_git(2024, 13)
