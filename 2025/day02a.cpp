f00e630b75b5798cb549c0598876fc9dfd8d3232
Caspar Verhey <caspar@verhey.net>
2025-12-03 11:25:47 +0100

    day02 cleanup

--- 2025/day02a.cpp
+++ 2025/day02a.cpp
@@ -2,23 +2,11 @@
 #include <sstream>
 #include <numeric> // std::accumulate
 
-void print(const std::vector<std::string> v) {
-    for(const auto& el : v)
-        std::cout << el << ' ';
-    std::cout << '\n';
-}
-
-void print(const std::vector<int> v) {
-    for(const auto& el : v)
-        std::cout << el << ' ';
-    std::cout << '\n';
-}
-
-void print(const std::vector<long long> v) {
-    for(const auto& el : v)
-        std::cout << el << ' ';
-    std::cout << '\n';
-}
+struct Range {
+    long long start;
+    long long end;
+    int width;
+};
 
 std::vector<std::string> split(std::stringstream& ss, const char& sep) {
     std::vector<std::string> result;
@@ -33,17 +21,7 @@ std::vector<std::string> split(const std::string& s, const char& sep) {
     return split(ss, sep);
 }
 
-struct Range {
-    long long start;
-    long long end;
-    int width;
-};
-
-void print(const Range r) {
-    std::cout << "Range " << r.start << '-' << r.end << " (width " << r.width << ")\n";
-}
-
-// "1-10,20-110" -> { {start: 1, end: 9, length: 1}, {10,10,2}, {20,99,2}, {100,110,3} }
+// "1-10,20-110" -> { {start: 1LL, end: 9LL, length: 1}, {10,10,2}, {20,99,2}, {100,110,3} }
 std::vector<Range> split_into_ranges(const std::string& s) {
 
     std::vector<Range> result;
@@ -65,26 +43,6 @@ std::vector<Range> split_into_ranges(const std::string& s) {
         }
     }
     return result;
-}
-
-// 12 -> {1, 2, 6, 3, 4}
-std::vector<int> divisors(const int& n, bool sorted = false) {
-    std::vector<int> divisors;
-    
-    for (int i = 1; i <= std::sqrt(n); ++i) {
-        if (n % i == 0) {
-            divisors.push_back(i);
-            if (i != n / i && i != 1)
-                divisors.push_back(n / i);
-        }
-    }
-    if (sorted)
-        std::sort(divisors.begin(), divisors.end());
-    return divisors;
-}
-
-bool in_range(const Range& range, const long long& val) {
-    return val >= range.start && val <= range.end;
 }
 
 long long rep_str(const std::string& val, const int& rep) {
@@ -110,12 +68,9 @@ void insert_repetition(const Range& range, std::vector<long long>& repetitions, 
 
 void find_repetitions(const Range& range, const int& width, std::vector<long long>& reps) {
     int times = range.width / width;
-    if(times != 2)
-        return;
     std::string start = std::to_string(range.start).substr(0, width);
     std::string end = std::to_string(range.end).substr(0, width);
-    //std::cout << " repeating " << start << " - " << end << '\n';
-
+    
     if(start == end) {
         long long rep = rep_str(start, times);
         insert_repetition(range, reps, rep);
@@ -136,15 +91,9 @@ int main() {
     std::vector<Range> ranges = split_into_ranges(line);
 
     std::vector<long long> repetitions;
-    for (const Range& range : ranges) {
-        //print(range);
-        for (const auto width : divisors(range.width)) {
-            find_repetitions(range, width, repetitions);
-        }
-        //std::cout << '\n';
-    }
-
-    //print(repetitions);
+    for (const Range& range : ranges)
+        if(range.width % 2 == 0)
+            find_repetitions(range, range.width / 2, repetitions);
 
     long long sum = std::accumulate(repetitions.begin(), repetitions.end(), 0LL);
     std::cout << sum << '\n';
